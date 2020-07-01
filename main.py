bf0a81bc3bac6c808ef66e7dc7873aeac740c847
Aryan Regmi <aryan98@live.com>
2020-07-01 17:11:30 -0600

    Import necessary modules

---
+++ main.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+from statistics import mean
+from dateutil.relativedelta import relativedelta
+import pickle
+import os.path
+
+# ------------------------------------------------------------------------------------------------------------------- #
+# -----------------------------------------------Initialize Variables------------------------------------------------ #
